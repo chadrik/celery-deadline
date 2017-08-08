9bfa1f22f950873b26bb1b1c57593201d9174bd0
Chad Dombrova <chadrik@gmail.com>
2017-08-07 22:04:50 -0700

    Use mongo to deliver task messages to deadline.
    Topic queues were a poor fit, and we know we have mongo available.

--- celery_deadline.py
+++ celery_deadline.py
@@ -1,15 +1,20 @@
 import sys
 import os
 import os.path
-import json
 import tempfile
 import getpass
+import json
 
 from celery.app.amqp import AMQP
-from celery import bootsteps
 import kombu
 import kombu.pools
+import kombu.transport.virtual.base as base
+from kombu.utils.objects import cached_property
+
 import requests
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 # deadline organizational strategies:
 # BatchName  Job Name
@@ -23,16 +28,18 @@ import requests
 
 # ExtraInfoKeyValue0=TestID=344
 
-task_exchange = kombu.Exchange('tasks', type='topic')
-
 
 def enable_deadline_support(app, mongodb):
     app.amqp_cls = __name__ + ':DeadlineAMQP'
 
 
-def get_queue(job_id):
-    return kombu.Queue('deadline-%s' % job_id, exchange=task_exchange,
-                       routing_key='deadline.%s.*' % job_id)
+def get_collection(client):
+    return client.celery_deadline.job_tasks
+
+
+class DummyChannel(base.Channel):
+    def __init__(self, *args, **kwargs):
+        pass
 
 
 def submit_job(pulse_url, job_info, plugin_info, aux_files=None, auth=None):
@@ -69,39 +76,56 @@ class DeadlineProducer(kombu.Producer):
 
     def __init__(self, channel, *args, **kwargs):
         self.deadline_pulse_url = kwargs.pop('deadline_pulse_url', None)
+        self.deadline_mongo_url = kwargs.pop('deadline_mongo_url', None)
         super(DeadlineProducer, self).__init__(channel, *args, **kwargs)
+
+    @cached_property
+    def mongo_client(self):
+        return MongoClient(self.deadline_mongo_url)
 
     def publish(self, body, routing_key=None, delivery_mode=None,
                 mandatory=False, immediate=False, priority=0,
                 content_type=None, content_encoding=None, serializer=None,
                 headers=None, compression=None, exchange=None, retry=False,
-                retry_policy=None, declare=[], **properties):
+                retry_policy=None, declare=None, expiration=None,
+                **properties):
         # FIXME:
         # detect if deadline was requested based on properties. 
         # maybe this doubles as way of passing pulse URL?
         deadline_requested = True
-        if deadline_requested and self.deadline_pulse_url:
-            # retry is probably not compatible with deadline?
-            retry = False
-            # if self.topic_exchange is None:
-            #     self.topic_exchange = task_exchange(self.channel)
-            #     self.topic_exchange.declare()
-            # FIXME: only call this once?
-            self.maybe_declare(task_exchange)
-            job_info = properties.pop('job_info', {}).copy()
+        if deadline_requested and self.deadline_pulse_url and self.deadline_mongo_url:
+            channel = DummyChannel()
+            headers = {} if headers is None else headers
+            compression = self.compression if compression is None else compression
+
+            if expiration is not None:
+                properties['expiration'] = str(int(expiration * 1000))
+
+            body, content_type, content_encoding = self._prepare(
+                body, serializer, content_type, content_encoding,
+                compression, headers)
+
+            message = channel.prepare_message(
+                body, priority, content_type,
+                content_encoding, headers, properties,
+            )
+            job_info = properties.get('job_info', {}).copy()
             job_id = self._submit_deadline_job(job_info, headers)
-            print "JOBID", job_id, body
-            routing_key = 'deadline.%s.0' % job_id
-            self.maybe_declare(get_queue(job_id))
-            exchange = task_exchange
-        return super(DeadlineProducer, self).publish(
-            body, routing_key, delivery_mode, mandatory, immediate, priority, 
-            content_type, content_encoding, serializer, headers, compression,
-            exchange, retry, retry_policy, declare, **properties)
+            print "JOBID", job_id, repr(body)
+            print message
+            tasks_col = get_collection(self.mongo_client)
+
+            tasks_col.update_one({"_id": ObjectId(job_id)},
+                                 {"$push": {"tasks": json.dumps(message)}},
+                                 upsert=True)
+        else:
+            return super(DeadlineProducer, self).publish(
+                body, routing_key, delivery_mode, mandatory, immediate, priority, 
+                content_type, content_encoding, serializer, headers, compression,
+                exchange, retry, retry_policy, declare, **properties)
 
     def _submit_deadline_job(self, job_info, headers):
         group_id = headers['group']
-
         # setup JobInfo
         job_info['Plugin'] = 'Celery'
         job_info['Frames'] = '1'
@@ -115,22 +139,39 @@ class DeadlineConsumer(kombu.Consumer):
     """
     Consumer that reads a single task then shuts down the worker
     """
+    def __init__(self, channel, *args, **kwargs):
+        self.deadline_mongo_url = kwargs.pop('deadline_mongo_url', None)
+        super(DeadlineConsumer, self).__init__(channel, *args, **kwargs)
 
-    # def consume(self, no_ack=None):
-    #     import kombu.utils
-    #     from kombu.message import Message
+    @cached_property
+    def mongo_client(self):
+        return MongoClient(self.deadline_mongo_url)
 
-    #     filepath = os.path.join('.', TASK_FILE)
-    #     print "consuming message", filepath
-    #     print os.getcwd()
-    #     with open(filepath, 'r') as f:
-    #         raw_message = json.loads(f.read())
-
-    #     raw_message['properties']['delivery_tag'] = kombu.utils.uuid()
-    #     # run the task:
-    #     self._receive_callback(raw_message)
-    #     print "done receive"
-    #     sys.exit(0)
+    def consume(self, no_ack=None):
+        import kombu.utils
+        tasks_col = get_collection(self.mongo_client)
+        # FIXME: get this elsewhere
+        curr_task_num = 0
+        job_id = os.environ['DEADLINE_JOB_ID']
+        # note: $slice is [skip, limit/count]
+        doc = tasks_col.find_one({'_id': ObjectId(job_id)},
+                                 {'tasks': {'$slice': [curr_task_num, 1]}})
+        raw_message = json.loads(doc['tasks'][0])
+        print "raw_message", raw_message
+        raw_message['properties']['delivery_tag'] = kombu.utils.uuid()
+        # override the channel so that we get a simple, consistent decoding scheme. otherwise,
+        # the behavior is dependent on the channel type
+        self.channel = DummyChannel()
+        # message = Message(payload=raw_message, channel=channel)
+        # print message.decode()
+        # print message.headers
+        # print self.on_message
+        # print getattr(self.channel, 'message_to_python', None)
+        # run the task:
+        print "running"
+        self._receive_callback(raw_message)
+        print "done receive"
+        sys.exit(0)
 
     def _receive_callback(self, message):
         print "MESSAGE", message
@@ -138,23 +179,36 @@ class DeadlineConsumer(kombu.Consumer):
         print "done receive"
         sys.exit(0)
 
-    def consume(self, no_ack=None):
-        import kombu.utils
-        from kombu.message import Message
+    # def consume(self, no_ack=None):
+    #     import kombu.utils
+    #     from kombu.message import Message
 
-        job_id = os.environ['DEADLINE_JOB_ID']
-        print "JOBID", job_id
-        queue = get_queue(job_id)
-        self._basic_consume(queue.bind(self.channel), no_ack=no_ack, nowait=True)
+    #     job_id = os.environ['DEADLINE_JOB_ID']
+    #     print "JOBID", job_id
+    #     queue = get_queue(job_id)
+    #     self._basic_consume(queue.bind(self.channel), no_ack=no_ack, nowait=True)
 
 
 class DeadlineAMQP(AMQP):
+    @cached_property
+    def _mongo_url(self):
+        conf = self.app.conf
+        mongo_url = conf.get('deadline_mongo_url')
+        print "mongo", mongo_url
+        if mongo_url:
+            return mongo_url
+        if conf.result_backend and conf.result_backend.startswith('mongodb://'):
+            return conf.result_backend
+        return
+
     def Consumer(self, *args, **kwargs):
         # FIXME: check for deadline env var
+        kwargs['deadline_mongo_url'] = self._mongo_url
         return DeadlineConsumer(*args, **kwargs)
 
     def Producer(self, *args, **kwargs):
         kwargs['deadline_pulse_url'] = self.app.conf.get('deadline_pulse_url')
+        kwargs['deadline_mongo_url'] = self._mongo_url
         return DeadlineProducer(*args, **kwargs)
 
     @property
--- testapp.py
+++ testapp.py
@@ -3,11 +3,14 @@ from celery import Celery
 app = Celery('testapp')
 # app.conf.broker_url = 'redis://localhost:6379/0'
 app.conf.broker_url = "amqp://guest:guest@localhost:5672//"
-app.conf.result_backend = 'redis://localhost:6379/0'
+# app.conf.result_backend = 'redis://localhost:6379/0'
+app.conf.result_backend = 'mongodb://macbook-pro-4.local:27090'
 app.amqp_cls = 'celery_deadline:DeadlineAMQP'
 app.conf.deadline_pulse_url = 'http://MacBook-Pro-4.local:8082'
+# app.conf.deadline_mongo_url = 'mongodb://macbook-pro-4.local:27090'
+
 # from celery_deadline import enable_deadline_support
-# enable_deadline_support(app, 'mongodb://')
+# enable_deadline_support(app, 'mongodb://macbook-pro-4.local')
 
 
 @app.task
@@ -23,6 +26,7 @@ def test():
     job = group([add.s(2, 2), add.s(4, 4)])
 
     result = job.apply_async(job_info={})
+    print "waiting for results:"
     for x in result.iterate():
         print "result is:", x
 
