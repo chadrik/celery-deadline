3c90e332f756dd6a8773410f6cc697ce4cbaede4
Chad Dombrova <chadrik@gmail.com>
2017-08-08 14:51:19 -0700

    Read the celery app from the celery task before executing the worker.

--- repo/plugins/Celery/Celery.py
+++ repo/plugins/Celery/Celery.py
@@ -1,11 +1,12 @@
-import os
-import re
-
+import json
 from System.IO import *
 from System.Text.RegularExpressions import *
-
-from Deadline.Scripting import *
 from Deadline.Plugins import *
+
+from Deadline.Scripting import ClientUtils, RepositoryUtils
+from MongoDB.Driver import MongoClient
+from MongoDB.Driver.Builders import Query, Fields
+from MongoDB.Bson import ObjectId, BsonExtensionMethods, BsonString, BsonObjectId
 
 
 def GetDeadlinePlugin():
@@ -14,6 +15,15 @@ def GetDeadlinePlugin():
 
 def CleanupDeadlinePlugin(deadlinePlugin):
     deadlinePlugin.Cleanup()
+
+
+def GetTaskCollection():
+    connStr = RepositoryUtils.GetDatabaseConnectionString()
+    urls = connStr.strip('()').split(',')
+    url = urls[0]
+    client = MongoClient('mongodb://' + url)
+    db = client.GetServer().GetDatabase('celery_deadline')
+    return db.GetCollection('job_tasks')
 
 
 class CeleryPlugin(DeadlinePlugin):
@@ -41,10 +51,28 @@ class CeleryPlugin(DeadlinePlugin):
         self.PluginType = PluginType.Simple
         self.SetEnvironmentVariable("DEADLINE_JOB_ID", str(self.GetJob().JobId))
 
+    def GetCeleryTasks(self):
+        collection = GetTaskCollection()
+        id = BsonObjectId(ObjectId.Parse(self.GetJob().JobId))
+        query = Query.EQ('_id', id)
+        # FIXME: get current frame
+        currentFrame = 0
+        # FIXME: support packet size > 1?
+        packetSize = 1
+        cursor = collection.Find(query).SetFields(Fields.Slice('tasks', currentFrame, packetSize)).SetLimit(1)
+        doc = list(cursor)[0]
+        results = []
+        for task in doc.GetValue('tasks'):
+            results.append(task.ToString())
+        return results
+
     def RenderExecutable(self):
         return 'celery'
 
     def RenderArgument(self):
-        app = 'testapp'
+        tasks = self.GetCeleryTasks()
+        # FIXME: support packet size > 1?
+        task = tasks[0]
+        app = json.loads(task)['headers']['task'].rsplit('.', 1)[0]
         arguments = '-A %s worker -l debug -P solo --without-gossip --without-mingle --without-heartbeat' % app
         return arguments
